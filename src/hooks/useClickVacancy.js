fa95c8ca83fb4e3fec820e1d9ce190bf2671c0e8
Дмитрий <dmitrykertsman@mail.ru>
2025-07-28 20:50:48 +0500

    deleted unused vars

--- src/hooks/useClickVacancy.js
+++ src/hooks/useClickVacancy.js
@@ -5,9 +5,6 @@ export const useClickVacancy = () => {
 	const { setVacancyId } = useVacancyStore();
 
 	const handleClickVacancy = e => {
-		const link = e.target.closest('a');
-		const linkUrl = new URL(link.href);
-		const pathname = linkUrl.pathname;
 		const id = getVacancyIdFromPath();
 
 		setVacancyId(id);
